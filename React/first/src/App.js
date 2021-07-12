b541fce01297d51fea8d8ed8bb893153cd11820a
asmaalasaada <asma.alsaada96@gmail.com>
2021-07-12 17:08:29 +0300

    first

--- React/first/src/App.js
+++ React/first/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import List from './components/List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello Dojo 
-        </h1>
-         <h2>Things I need to do </h2>
-        
-        
-        <ul>
-        <li> Learn React</li>
-        <li> Climb Mt Everest</li>
-        <li> Run a marathon</li>
-        <li> Feed the dogs</li>
-        </ul>
-        
-        
-        </header>   
+    <div className="App">   
+    <Header />
+    <List/>
     </div>
   );
 }
---
+++ React/first/src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <div>
+            <h3>Things I need to do</h3>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mount Everest</li>
+                <li>Run a Marathon</li>
+                <li>Feed the Dogs</li>
+            </ul>
+        </div>
+    );
+}
+
+
+
+export default List;
